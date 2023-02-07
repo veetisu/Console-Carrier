aabc8883a768d11e2c6f686027936cb3b4ad1553
AnttoS <anttosalo3@gmail.com>
2023-02-07 15:00:23 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import math
+
+class Carrier():
+    pass
+
+class Airplane():
+    pass
+
+class App():
+    def __init__(self):
+        self.running = True
+        self.i = 0
+        while self.running:
+            pass
+
+app = App()
