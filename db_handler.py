1a4437fc39c0015c481e07f7a8e6140ebbee54e1
AnttoS <anttosalo3@gmail.com>
2023-02-08 14:12:38 +0200

    Initializing the carrier

--- db_handler.py
+++ db_handler.py
@@ -1,16 +1,17 @@
 import mariadb
 import sys
 import os
-try:
-    conn = mariadb.connect(
-        user="root",
-        password= os.environ.get("DBPASS") or "admin",
-        host="localhost",
-        port=3306,
-        database="flight_game"
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
 class Db_handler():
+    def __init__(self):
+        try:
+            conn = mariadb.connect(
+                user="root",
+                password= os.environ.get("DBPASS") or "admin",
+                host="localhost",
+                port=3306,
+                database="flight_game"
+            )
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
     pass
--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import math
-from db_handler import DBHandler
+import random
+import time
+#from db_handler import DBHandler
 
 class Carrier():
     def __init__(self, name):
         #Creates a new Carrier for the player
         self.name = name
-    
+        self.airplanes = []
+
 
 class Airplane():
-    pass
+    def __init__(self, type):
+        pass
 
 class App():
     def __init__(self):
         self.running = True
-        while self.running:
-            #Initializing the game 
-            carrier = Carrier()
+        #Initializing the game/setting up the carrier
+        print("Welcome to Console Carrier!")
+        time.sleep(1)
+        input("Are you ready to start your journey as a airline manager? (Press Enter to start)")
+        time.sleep(1)
+        name_input = input("Enter a name for your airline: ")
+        carrier = Carrier(name_input)
+        #Lines for printing messages to player
+        self.lines = {
+            
+        }
+    def get_random_line(self,scenario):
+        return random.choice(self.lines[scenario])
+        
+    def run(self):
+            while self.running:
+                pass
+            
 
 app = App()
+app.run()
