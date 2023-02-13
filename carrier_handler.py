37d9d3a6006a6ccf60d40cd3230adc87a51f0250
AnttoS <anttosalo3@gmail.com>
2023-02-13 12:19:15 +0200

    Comments

--- carrier_handler.py
+++ carrier_handler.py
@@ -1,9 +1,14 @@
 class Carrier():
+    #TI = To Implement
     def __init__(self, name, first_plane):
         #Creates a new Carrier for the player
         self.name = name
         self.airplanes = []
-
+        #TI: Init the class with resources
+    
+    #TI: Method to add new plane
+    #TI: Method to add / remove given amount of resources
+    #TI: Method to print players resources, return nice string for printing
 
 class Airplane():
     def __init__(self, type):
