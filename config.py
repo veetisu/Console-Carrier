4d0e6624358c28ba478afebaff4cf260339f42f1
Antto Salo <anttosalo3@gmail.com>
2023-02-16 17:29:45 +0200

    Added fuel consumption

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 MAX_AIRPORTS_PER_SEARCH = 4
-STARTING_FUEL = 100
+STARTING_FUEL = 1000
 STARTING_MONEY = 100
-#range in nautical miles!!
+
+
+
 PLANES = {
     "C172" : {
+        "name": "Cessna-172",
+        #range in nautical miles!!
         "range": 640,
-        "passenger_capacity":3
+        "passenger_capacity":3,
+        #Litres per km
+        "fuel_consumption": 1
     }
 }
